256e2058265ee924f966bbfb67c2a0b198273844
Adrian Skowron <adrian.skowron@mitron.com>
2018-02-17 00:22:40 +0100

    Initial relase.

--- lighthttpd/dockerfiles/install.sh
+++ lighthttpd/dockerfiles/install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# https://bugs.launchpad.net/ubuntu/+source/debian-installer/+bug/568704
+unset http_proxy
+
+
 # https://kubernetes.io/docs/setup/independent/install-kubeadm/
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
@@ -6,3 +11,7 @@ deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -yq kubelet kubeadm kubectl
+
+sed -i 's/PermitRootLogin .*/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+
