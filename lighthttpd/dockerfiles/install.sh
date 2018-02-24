31fadded3040e96c9a9c68ee2bd7b150d753d2a5
Adrian Skowron <adrian.skowron@mitron.com>
2018-02-24 22:48:59 +0100

    Removing kubernetes instalation

--- lighthttpd/dockerfiles/install.sh
+++ lighthttpd/dockerfiles/install.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-
-# https://bugs.launchpad.net/ubuntu/+source/debian-installer/+bug/568704
 unset http_proxy
-
-
-# https://kubernetes.io/docs/setup/independent/install-kubeadm/
-wget -qO - https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -yq kubelet kubeadm kubectl docker.io
-
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install -yq docker.io
 sed -i 's/PermitRootLogin .*/PermitRootLogin yes/' /etc/ssh/sshd_config
-
-
