18947f54f33f76b3bdcbadd7a7d62f9631931f80
Adrian Skowron <adrian.skowron@mitron.com>
2018-03-09 19:19:13 +0100

    Checkign

---
+++ lighthttpd/dockerfiles/install-mesos-master.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# https://www.digitalocean.com/community/tutorials/how-to-configure-a-production-ready-mesosphere-cluster-on-ubuntu-14-04
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | tee /etc/apt/sources.list.d/mesosphere.list
+DEBIAN_FRONTEND=noninteractive apt-get -y update && DEBIAN_FRONTEND=noninteractive apt-get install -y zookeeper mesos marathon chronos
+
+
+# Set up the Zookeeper Connection Info for Mesos
+
+serverhostname1=node1
+serverhostname2=node2
+serverhostname3=node3
+
+serverip1=10.6.1.101
+serverip2=10.6.1.102
+serverip2=10.6.1.103
+
+quorum=2
+
+cat > /etc/mesos/zk <<EOF
+zk://$serverip1:2181,$serverip2:2181,$serverip3:2181/mesos
+EOF
+
+
+
+# Configure the Master Server's Zookeeper Configuration
+case $(hostname) in
+  $serverhostname1)
+    serverid=1
+    serverip=$serverip1
+  ;;
+  $serverhostname2) 
+    serverid=2
+    serverip=$serverip2
+  ;;
+  $serverhostname3)
+    serverid=3
+    serverip=$serverip3
+  ;;
+  *) 
+    echo NODE HOSTNAME FOR MASTER SERVER MISMATCH FIX CONFIGURATION
+    exit 3
+  ;;
+esac
+
+cat > /etc/zookeeper/conf/myid <<EOF
+$serverid
+EOF
+
+
+cat > /etc/zookeeper/conf/zoo.cfg <<EOF
+tickTime=2000
+initLimit=10
+syncLimit=5
+dataDir=/var/lib/zookeeper
+clientPort=2181
+server.1=10.6.1.101:2888:3888
+server.2=10.6.1.102:2888:3888
+server.3=10.6.1.103:2888:3888
+EOF
+
+cat > /etc/mesos-master/quorum <<EOF
+$quorum
+EOF
+
+cat > /etc/mesos-master/ip  <<EOF
+$serverip
+EOF
+
+cat > /etc/mesos-master/hostname <<EOF
+$serverip
+EOF
+
+# Configure Marathon on the Master Servers
+mkdir -p /etc/marathon/conf
+
+cat > /etc/marathon/conf/hostname <<EOF
+$serverip
+EOF
+
+
+cat > /etc/marathon/conf/master <<EOF
+zk://$serverip1:2181,$serverip2:2181,$serverip3:2181/mesos
+EOF
+
+cat > /etc/marathon/conf/zk <<EOF
+zk://$serverip1:2181,$serverip2:2181,$serverip3:2181/marathon
+EOF
+
--- lighthttpd/dockerfiles/install.sh
+++ lighthttpd/dockerfiles/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 unset http_proxy
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get install -yq docker.io
 sed -i 's/PermitRootLogin .*/PermitRootLogin yes/' /etc/ssh/sshd_config
 
 
@@ -12,4 +11,90 @@ cat > /etc/docker/daemon.json <<EOF
 }
 EOF
 
-echo "10.6.1.1 private.registry.local" >> /etc/hosts
+
+cat > /etc/hosts <<EOF
+127.0.0.1       localhost
+127.0.1.1       $(hostname)
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+
+10.6.1.101 s1
+10.6.1.102 s2
+10.6.1.103 s3
+10.6.1.104 s4
+10.6.1.105 s5
+10.6.1.1 private.registry.local
+
+EOF
+function run_zk(){
+  # https://github.com/sekka1/mesosphere-docker#multi-node-setup
+  HOST_IP_1=10.6.1.101
+  HOST_IP_2=10.6.1.103
+  HOST_IP_3=10.6.1.105
+  NODE_ID=$(hostname | sed s@node@@g)
+
+
+   docker run -d \
+     -p 2181:2181 \
+     -p 2888:2888 \
+     -p 3888:3888 \
+     -e ADDITIONAL_ZOOKEEPER_1=ADDITIONAL_ZOOKEEPER_1=server.1=${HOST_IP_1}:2888:3888 \
+     -e ADDITIONAL_ZOOKEEPER_2=ADDITIONAL_ZOOKEEPER_1=server.2=${HOST_IP_2}:2888:3888 \
+     -e ADDITIONAL_ZOOKEEPER_3=ADDITIONAL_ZOOKEEPER_1=server.3=${HOST_IP_3}:2888:3888  \
+     -e SERVER_ID=${NODE_ID} \
+     --name zookeeper private.registry.local:443/cluster/zookeeper
+}
+
+
+
+function run_master(){
+
+
+docker run -d --restart=always --net=host \
+  -e MESOS_PORT=5050 \
+  -e MESOS_ZK=zk://10.6.1.101:2181,10.6.1.103:2181,10.6.1.105:2181/mesos \
+  -e MESOS_QUORUM=2 \
+  -e MESOS_REGISTRY=in_memory \
+  -e MESOS_LOG_DIR=/var/log/mesos \
+  -e MESOS_WORK_DIR=/var/tmp/mesos \
+  -v "$(pwd)/log/mesos:/var/log/mesos" \
+  -v "$(pwd)/tmp/mesos:/var/tmp/mesos" \
+  private.registry.local:443/mesosphere/mesos-master:0.28.0-2.0.16.ubuntu1404
+
+}
+
+function run_slave(){
+
+docker run -d --restart=always --net=host --privileged \
+  -e MESOS_PORT=5051 \
+  -e MESOS_MASTER=zk://10.6.1.101:2181,10.6.1.103:2181,10.6.1.105:2181/mesos \
+  -e MESOS_SWITCH_USER=0 \
+  -e MESOS_CONTAINERIZERS=docker,mesos \
+  -e MESOS_LOG_DIR=/var/log/mesos \
+  -e MESOS_WORK_DIR=/var/tmp/mesos \
+  -v "$(pwd)/log/mesos:/var/log/mesos" \
+  -v "$(pwd)/tmp/mesos:/var/tmp/mesos" \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /cgroup:/cgroup \
+  -v /sys:/sys \
+  -v /usr/local/bin/docker:/usr/local/bin/docker \
+  private.registry.local:443/mesosphere/mesos-slave:0.28.0-2.0.16.ubuntu1404
+
+}
+
+
+
+case $(hostname) in
+  "node1"|"node3"|"node5")
+    run_zk
+    run_master
+    run_slave
+  ;;
+  *)
+    run_zk
+    run_slave
+  ;;
+esac
