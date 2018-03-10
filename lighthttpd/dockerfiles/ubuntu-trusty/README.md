0553ba19c1494bb922588efd22329203fd2ab8a9
Adrian Skowron <adrian.skowron@mitron.com>
2018-03-10 14:31:05 +0100

    Deleted ubuntu-trusty. Added docker-marathon... testing...

--- lighthttpd/dockerfiles/ubuntu-trusty/README.md
+++
@@ -1,4 +0,0 @@
-#
-
-http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/linux
-http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/initrd.gz
---
+++ lighthttpd/dockerfiles/ubuntu-xenial/mesos.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+
+
+function run_zk(){
+  # https://github.com/sekka1/mesosphere-docker#multi-node-setup
+  HOST_IP_1=10.6.1.101
+  HOST_IP_2=10.6.1.103
+  HOST_IP_3=10.6.1.105
+
+
+  docker run -d --restart=always --net=host \
+     -e ADDITIONAL_ZOOKEEPER_1="server.1=${HOST_IP_1}:2888:3888" \
+     -e ADDITIONAL_ZOOKEEPER_2="server.2=${HOST_IP_2}:2888:3888" \
+     -e ADDITIONAL_ZOOKEEPER_3="server.3=${HOST_IP_3}:2888:3888"  \
+     -e SERVER_ID=${NODE_ID} \
+     --name zookeeper private.registry.local:443/cluster/zookeeper
+}
+
+
+
+function run_master(){
+
+
+docker run -d --restart=always --net=host \
+  -e MESOS_PORT=5050 \
+  -e MESOS_ZK=zk://10.6.1.101:2181,10.6.1.103:2181,10.6.1.105:2181/mesos \
+  -e MESOS_IP=${SERVERIP}  \
+  -e MESOS_QUORUM=2 \
+  -e MESOS_HOSTNAME=$(hostname) \
+  -e MESOS_REGISTRY=in_memory \
+  -e MESOS_LOG_DIR=/var/log/mesos \
+  -e MESOS_WORK_DIR=/var/tmp/mesos \
+  -v "$(pwd)/log/mesos:/var/log/mesos" \
+  -v "$(pwd)/tmp/mesos:/var/tmp/mesos" \
+  --name master \
+  --entrypoint mesos-master \
+  private.registry.local:443/mesosphere/mesos:1.5.0 \
+  --registry=in_memory
+
+}
+
+function run_slave(){
+
+docker run -d --restart=always --net=host --privileged \
+  -e MESOS_SWITCH_USER=0 \
+  -e MESOS_HOSTNAME=$(hostname) \
+  -e MESOS_IP=${SERVERIP}  \
+  -e MESOS_CONTAINERIZERS=docker,mesos \
+  -e MESOS_LOG_DIR=/var/log/mesos \
+  -v "$(pwd)/log/mesos:/var/log/mesos" \
+  -v "$(pwd)/tmp/mesos:/var/tmp/mesos" \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /cgroup:/cgroup \
+  -v /sys:/sys \
+  --name slave \
+  --entrypoint mesos-slave \
+  private.registry.local:443/mesosphere/mesos:1.5.0 \
+  --no-systemd_enable_support --master=zk://10.6.1.101:2181,10.6.1.103:2181,10.6.1.105:2181/mesos --work_dir=/var/tmp/mesos
+}
+
+function run_marathon(){
+
+docker run -d \
+           --restart=always \
+           --net=host \
+           --privileged \
+           --name marathon \
+           --entrypoint marathon \
+           private.registry.local:443/cluster/marathon \
+           --master=zk://10.6.1.101:2181,10.6.1.103:2181,10.6.1.105:2181/mesos --zk zk://10.6.1.101:2181,10.6.1.103:2181,10.6.1.105:2181/marathon
+}
+
+docker stop zookeeper master slave marathon
+docker rm zookeeper master slave marathon
+
+
+case $(hostname) in
+  "node1")
+    SERVERIP=10.6.1.101
+    NODE_ID=1
+    run_zk
+    run_master
+    run_slave
+    run_marathon
+  ;;
+  "node3")
+    SERVERIP=10.6.1.103
+    NODE_ID=2
+    run_zk
+    run_master
+    run_slave
+    run_marathon
+  ;;
+  "node5")
+    SERVERIP=10.6.1.105
+    NODE_ID=3
+    run_zk
+    run_master
+    run_slave
+    run_marathon
+  ;;
+  "node2")
+    SERVERIP=10.6.1.102
+    run_slave
+  ;;
+  "node4")
+    SERVERIP=10.6.1.104
+    run_slave
+  ;;
+esac
